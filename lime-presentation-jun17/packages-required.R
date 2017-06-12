74fd54074773f71d1663b07c725cff051c93389a
iBrianCarter <bcarter_ie@yahoo.co.uk>
2017-06-12 20:27:38 +0100

    experiments and pres

--- lime-presentation-jun17/packages-required.R
+++ lime-presentation-jun17/packages-required.R
@@ -1,2 +1,8 @@
 install.packages("revealjs", type = "source")
 devtools::install_github('yihui/xaringan')
+devtools::install_github("thomasp85/lime")
+install.packages("RSNSS") #for nueral nets
+install.packages("gbm") #gradient boosting
+install.packages("xgboost") #extreme gradient boosting (paraellel)
+install.packages("corrplot")
+install.packages("highlight")
